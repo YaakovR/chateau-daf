c065c835adc941d80b2623cd1372db14c7748f05
YaakovR <yaakovr@ltccs.com>
2017-09-25 10:20:17 -0400

    Update 2017_09_10_180602_create_mesechtos_table.php
    Added dropIfExists statement.

--- database/migrations/2017_09_10_180602_create_mesechtos_table.php
+++ database/migrations/2017_09_10_180602_create_mesechtos_table.php
@@ -13,6 +13,7 @@ class CreateMesechtosTable extends Migration
      */
     public function up()
     {
+        Schema::dropIfExists('mesechtos');
         Schema::create('mesechtos', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
