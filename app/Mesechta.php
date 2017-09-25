9b6ad8e4cdd6c013570227fbc5b1573cae021e2f
Yaakov Rosenfeld <yaakovrosenfeld@gmail.com>
2017-09-24 22:42:33 -0400

    Updated some dependencies. Created Mesechta model. Fetching data via the new model.

---
+++ app/Mesechta.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Mesechta extends Model
+{
+    protected $table = 'mesechtos';
+}
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,6 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Mesechta;
 
 Route::get('/', function () {
     return view('index');
@@ -18,17 +9,7 @@ Route::get('/', function () {
 
 Route::get('/mesechtos', function () {
 
-//    $mesechtos = [
-//        'Brachos',
-//        'Shabbos',
-//        'Eruvin',
-//        'Pesachim',
-//        'Shekalim'
-//    ];
-
-    $mesechtos = \Illuminate\Support\Facades\DB::table('mesechtos')->get();
-
-//    return $mesechtos;
+    $mesechtos = Mesechta::all();
 
     return view('mesechtos', compact('mesechtos'));
 });
